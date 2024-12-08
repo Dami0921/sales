64b23fc1df0e3effceba058d522d25e1007f6b7c
Dami0921 <damiancorreatusarma248@gmail.com>
2024-12-07 19:28:03 -0500

    add data context

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    // DataContext hereda de la clase Dbcontext
+    public class DataContext : DbContext
+    {
+
+        // ctor para conectar a la BD
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+
+        // propiedad para mapear tablas
+        public DbSet<Country> countries { get; set; }
+
+        // validacion para duplicados
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        // propiedad ID
+        public int Id { get; set; }
+
+        // atributo name pais
+        [Display(Name = "País")]
+        // atributo pais obligatorio
+        [Required(ErrorMessage = "El campo {0} es obligatorrio.")]
+        // atributo carateres maximos para el pais
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+
+        // propiedad nombre pais
+        public string Name { get; set; } = null!;
+    }
+}
